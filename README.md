9c150384a0694e209cdb367a766fada023377841
Matthew Burket <Matthew@assignitapp.com>
2011-02-08 23:37:36 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Open Tests
+## An Ruby on Rails Testing site
+Open test is proudly open source.
+##Credits
+Matthew Burket <Matthew@AssignItApp.com || Lead Developer 
+
+##License
+This software is released under the GPL v3 license.
+full license in docs/license
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Num1::Application.config.secret_token = '589a2a7b5305ad28a2949e1f23e4e290228a91a262c5eeefea0bbee596b0dbc2c9fbb8fd0fa2a73b528e7ab8006d054cdf483234abb3b3e7bbc2be7eb9b2012c'
