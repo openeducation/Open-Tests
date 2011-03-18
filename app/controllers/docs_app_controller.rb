71e5979618e738ec3ff584a5ce0dedb4b4c65b0a
Matthew Burket <Mab879@msn.com>
2011-03-18 10:44:22 -0500

    Added some insite docs

--- app/controllers/docs_app_controller.rb
+++ app/controllers/docs_app_controller.rb
@@ -1,11 +1,14 @@
 class DocsAppController < ApplicationController
   def index
+    @title = "Open Education Docs"
   end
 
   def CREDITS
+    @title = "CREDITS"
   end
 
   def LICENSE
+    @title = "LICENSE"
   end
 
 end
